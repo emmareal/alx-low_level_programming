caad0e145120797a2f349fa7702fe746315aed95
emmareal <chinwezeemmanuel@gmail.com>
2022-10-13 13:52:22 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry
+ * main - Printing
  * Return: Always 0
  */
 int main(void)
 {
-	puts("\n"Programming is like building a multi lingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
